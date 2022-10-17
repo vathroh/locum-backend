2a4670a6d741265fe20623afbdcd9b9c8436f45a
fathur <vathroh@gmail.com>
2022-10-17 14:42:55 +0700

    update edit profile

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,24 +2,47 @@ const User = require("../models/User.js");
 
 const getUsers = async (req, res) => {
     try {
-        const users = await User.find()
-            .then((data) => {
-                res.json(data);
-            })
+        const users = await User.find().then((data) => {
+            res.json(data);
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}
+};
 
 const getUserById = async (req, res) => {
     try {
-        const user = await User.findById(req.params.id)
-            .select({ full_name: 1, profile_pict: 1, phone_number: 1, email: 1, role: 1, about_me: 1, certification: 1, preferences: 1, resume: 1, achievement: 1 });
-        res.json(user);
+        const user = await User.findById(req.params.id).select({
+            full_name: 1,
+            profile_pict: 1,
+            phone_number: 1,
+            email: 1,
+            role: 1,
+            about_me: 1,
+            certification: 1,
+            preferences: 1,
+            resume: 1,
+            achievement: 1,
+        });
+
+        const data = {
+            _id: user._id,
+            full_name: user.full_name ?? "",
+            profile_pict: user.profile_pict ?? "",
+            phone_number: user.phone_number ?? "",
+            email: user.email ?? "",
+            role: user.role ?? "",
+            about_me: user.about_me ?? "",
+            certification: user.certification ?? [],
+            preferences: user.preferences ?? [],
+            resume: user.resume ?? "",
+            achievement: user.achievement ?? [],
+        };
+        res.json(data);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-}
+};
 
 const saveUser = async (req, res) => {
     const user = new User(req.body);
@@ -29,18 +52,36 @@ const saveUser = async (req, res) => {
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
-}
+};
 
 const updateUser = async (req, res) => {
-    const cekId = await User.findById(req.params.id);
-    if (!cekId) return res.status(404).json({ message: "The data not found" });
     try {
-        const updatedUser = await User.updateOne({ _id: req.params.id }, { $set: req.body });
+        const cekId = await User.findById(req.params.id);
+        if (!cekId) {
+            return res.status(404).json({ message: "The data not found" });
+        }
+
+        if (req.body.full_name) {
+            cekId.full_name = req.body.full_name;
+        }
+
+        if (req.body.about_me) {
+            cekId.about_me = req.body.about_me;
+        }
+
+        if (req.body.profile_pict) {
+            cekId.profile_pict = req.body.profile_pict;
+        }
+
+        const updatedUser = await User.updateOne(
+            { _id: req.params.id },
+            { $set: req.body }
+        );
         res.status(200).json(updatedUser);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
-}
+};
 
 const deleteUser = async (req, res) => {
     const cekId = await User.findById(req.params.id);
@@ -51,6 +92,6 @@ const deleteUser = async (req, res) => {
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
-}
+};
 
-module.exports = { getUsers, getUserById, saveUser, updateUser, deleteUser }
+module.exports = { getUsers, getUserById, saveUser, updateUser, deleteUser };
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,37 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const Job = require("../models/Job.js");
-const { authFirebaseMiddleware, authJwtMiddleware, verifyIdToken } = require('../middleware/authMiddleware')
-const { DateTime, Duration } = require('luxon')
+const {
+    authFirebaseMiddleware,
+    authJwtMiddleware,
+    verifyIdToken,
+} = require("../middleware/authMiddleware");
+const { DateTime, Duration } = require("luxon");
 
-const multer = require('multer')
-const path = require("path")
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "public/images/jobs")
+        cb(null, "public/images/jobs");
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-})
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
-router.post('/upload', upload.single('image'), (req, res) => {
-
+router.post("/upload", upload.single("image"), (req, res) => {
     console.log(req.body);
-    res.send(req.body)
-})
+    res.send(req.body);
+});
 
-router.post('/jobs', upload.single('image'), async (req, res) => {
-    let data = req.body
+router.post("/jobs", upload.single("image"), async (req, res) => {
+    let data = req.body;
     data.image = "/" + req.file?.destination + "/" + req.file?.filename;
     // return res.json(data)
     console.log(req.file?.destination);
-    data.work_time_start = DateTime.fromISO(req.body.date + "T" + req.body.work_time_start, { zone: "Asia/Singapore" }).toMillis()
-    data.work_time_finish = DateTime.fromISO(req.body.date + "T" + req.body.work_time_finish, { zone: "Asia/Singapore" }).toMillis()
-    data.date = DateTime.fromISO(req.body.date, { zone: "Asia/Singapore" }).toMillis();
+    data.work_time_start = DateTime.fromISO(
+        req.body.date + "T" + req.body.work_time_start,
+        { zone: "Asia/Singapore" }
+    ).toMillis();
+    data.work_time_finish = DateTime.fromISO(
+        req.body.date + "T" + req.body.work_time_finish,
+        { zone: "Asia/Singapore" }
+    ).toMillis();
+    data.date = DateTime.fromISO(req.body.date, {
+        zone: "Asia/Singapore",
+    }).toMillis();
 
     const job = new Job(data);
 
@@ -41,12 +52,14 @@ router.post('/jobs', upload.single('image'), async (req, res) => {
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
-})
+});
 
-router.get('/verify', verifyIdToken);
+router.get("/verify", verifyIdToken);
 
-router.get('/', authFirebaseMiddleware, (req, res) => {
-    res.send('<center><h1 style="margin-top:200px;">Hello from LOCUM App.</h1></center>')
+router.get("/", authJwtMiddleware, (req, res) => {
+    res.send(
+        '<center><h1 style="margin-top:200px;">Hello from LOCUM App.</h1></center>'
+    );
 });
 
 module.exports = router;
