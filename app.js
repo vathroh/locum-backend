b3a78a2e969ff32b89797953182650eb524dc5e9
fathur <vathroh@gmail.com>
2022-11-23 12:15:46 +0700

    cors

--- app.js
+++ app.js
@@ -20,10 +20,26 @@ const io = new Server(server, {
     },
 });
 
+var allowedOrigins = [
+    "http://localhost:3000",
+    "https://app-staging.work-wiz.com",
+];
+
 app.use(
     cors({
-        origin: true,
         credentials: true,
+        origin: function (origin, callback) {
+            // allow requests with no origin
+            // (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg =
+                    "The CORS policy for this site does not " +
+                    "allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
     })
 );
 
