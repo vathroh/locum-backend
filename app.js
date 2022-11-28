3d3cec40eb6bbf90b6a0681769743a9b91a592d2
fathur <vathroh@gmail.com>
2022-11-28 20:59:22 +0700

    adding cors

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var allowedOrigins = [
 
 app.use(
   cors({
-    credentials: true,
+    // credentials: true,
     origin: function (origin, callback) {
       // allow requests with no origin
       // (like mobile apps or curl requests)
