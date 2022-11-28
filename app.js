2298e4c61c40c194fb4746a6a5c685092c5ff779
fathur <vathroh@gmail.com>
2022-11-28 20:49:37 +0700

    adding cors

--- app.js
+++ app.js
@@ -21,10 +21,28 @@ const io = new Server(server, {
   },
 });
 
+var allowedOrigins = [
+  "http://localhost:3000",
+  "https://app-staging.work-wiz.com",
+  "*.netlify.app",
+  "*",
+];
+
 app.use(
   cors({
     credentials: true,
-    origin: true,
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
 
