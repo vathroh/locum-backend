033d30325728880d2bc8d24c17e4cf3a6cb5b5b7
fathur <vathroh@gmail.com>
2022-12-01 13:00:26 +0700

    debug cronjob

--- services/cronJob/index.js
+++ services/cronJob/index.js
@@ -3,11 +3,11 @@ const { DateTime } = require("luxon");
 const { seedJobs } = require("../../faker/job");
 const { setUrgentJob } = require("./setUrgentJob");
 
-cronJob.schedule("0 0 0 * * *", () => {
-    const now = DateTime.now().setZone("Asia/Jakarta").toFormat("HH:mm:ss");
-    seedJobs();
+cronJob.schedule("0 3 * * * *", () => {
+  const now = DateTime.now().setZone("Asia/Jakarta").toFormat("HH:mm:ss");
+  seedJobs();
 });
 
 cronJob.schedule("0 0 19 * * *", () => {
-    setUrgentJob();
+  setUrgentJob();
 });
