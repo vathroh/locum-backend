959ff4999c15ca8515e5648f3ba4933445bcf7cf
fathur <vathroh@gmail.com>
2022-12-13 09:42:22 +0700

    change message output

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -20,8 +20,7 @@ router.get("/:conversationId", async (req, res) => {
     const limit = parseInt(req.query.limit) || 100;
     const totalPage = Math.ceil(totalRows / limit);
     const page = parseInt(req.query.page) - 1 || totalPage - 1;
-    // const offset = limit * page
-    const offset = limit * (page - 1) + (totalRows % limit);
+    const offset = limit * page - (totalRows % limit);
 
     return res.json({
       page: page + 1,
