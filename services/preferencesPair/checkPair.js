1aa59b0286deaf7304b02e28ad1b0ddb8dd3ff25
fathur <vathroh@gmail.com>
2022-12-21 14:42:15 +0700

    fixed check pair

--- services/preferencesPair/checkPair.js
+++ services/preferencesPair/checkPair.js
@@ -3,15 +3,17 @@ const { getPair } = require(".");
 const checkPair = async (preferences) => {
   const isExists = [];
 
-  const checking = preferences?.map(async (data) => {
-    const pair = await getPair(data);
+  if (preferences.length > 0) {
+    const checking = preferences?.map(async (data) => {
+      const pair = await getPair(data);
 
-    if (pair) {
-      pair.map((item) => {
-        if (preferences.includes(item)) isExists.push(item);
-      });
-    }
-  });
+      if (pair) {
+        pair.map((item) => {
+          if (preferences.includes(item)) isExists.push(item);
+        });
+      }
+    });
+  }
 
   if (checking) await Promise.all(checking);
 
