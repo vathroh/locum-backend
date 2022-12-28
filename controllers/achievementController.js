4b034e7914056206a9d9f98c24f1307394c25e0f
fathur <vathroh@gmail.com>
2022-12-28 21:36:46 +0700

    delete file & achievement

--- controllers/achievementController.js
+++ controllers/achievementController.js
@@ -1,62 +1,85 @@
 const Achievement = require("../models/Achievement");
 const { achievementLogger } = require("../services/logger/achievementLogger");
 const multer = require("multer");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public/images/user");
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
-    },
+  destination: (req, file, cb) => {
+    cb(null, "public/images/user");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage: storage });
 
 const getAchievement = async (req, res) => {
-    try {
-        const achievement = await Achievement.find({
-            user_id: req.params.userId,
-        });
+  try {
+    const achievement = await Achievement.find({
+      user_id: req.params.userId,
+    });
 
-        achievementLogger.info(`url: ${req.originalUrl}`);
+    achievementLogger.info(`url: ${req.originalUrl}`);
 
-        res.json(achievement);
-    } catch (error) {
-        achievementLogger.error(
-            `url: ${req.originalUrl}, error: ${error.message}, user:${req.user._id}`
-        );
+    res.json(achievement);
+  } catch (error) {
+    achievementLogger.error(
+      `url: ${req.originalUrl}, error: ${error.message}, user:${req.user._id}`
+    );
 
-        res.status(500).json({ message: error.message });
-    }
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const postAchievement = async (req, res) => {
-    try {
-        if (!req.file)
-            return res.status(404).json({ message: "The file is empty" });
+  try {
+    if (!req.file)
+      return res.status(404).json({ message: "The file is empty" });
 
-        data = {};
-        data.item = req.body.item;
-        data.user_id = req.params.userId;
-        data.file = "/" + req.file?.destination + "/" + req.file?.filename;
-        const achievement = new Achievement(data);
-        await achievement.save();
+    data = {};
+    data.item = req.body.item;
+    data.user_id = req.params.userId;
+    data.file = "/" + req.file?.destination + "/" + req.file?.filename;
+    const achievement = new Achievement(data);
+    await achievement.save();
 
-        achievementLogger.info(
-            `url: ${req.originalUrl}, uploaded successfully`
-        );
+    achievementLogger.info(`url: ${req.originalUrl}, uploaded successfully`);
 
-        res.json({
-            message: "The achievement has been inputed successfully.",
-        });
-    } catch (error) {
-        achievementLogger.error(
-            `url: ${req.originalUrl}, error: ${error.message}, user:${req.user._id}`
-        );
+    res.json({
+      message: "The achievement has been inputed successfully.",
+    });
+  } catch (error) {
+    achievementLogger.error(
+      `url: ${req.originalUrl}, error: ${error.message}, user:${req.user._id}`
+    );
 
-        res.status(500).json({ message: error.message });
-    }
+    res.status(500).json({ message: error.message });
+  }
 };
 
-module.exports = { getAchievement, postAchievement, upload };
+const deleteAchievement = async (req, res) => {
+  const cekId = await Achievement.findById(req.params.id);
+  if (!cekId) return res.status(404).json({ message: "Data not found." });
+
+  try {
+    if (fs.existsSync("." + cekId.file)) {
+      fs.unlink("." + cekId.file, (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  try {
+    const deleted = await Achievement.deleteOne({ _id: req.params.id });
+    res.status(200).json(deleted);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAchievement, postAchievement, deleteAchievement, upload };
--- routes/achievementRoutes.js
+++ routes/achievementRoutes.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 
 const {
-    upload,
-    getAchievement,
-    postAchievement,
+  upload,
+  getAchievement,
+  postAchievement,
+  deleteAchievement,
 } = require("../controllers/achievementController");
 
 router.get("/:userId", getAchievement);
+router.delete("/:id", deleteAchievement);
 router.post("/:userId", upload.single("achievement"), postAchievement);
 
 module.exports = router;
